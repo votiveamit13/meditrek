da60ef5dcabe78be9ad9de2f135e771d14918688
Amit Patel <votiveamit.web@gmail.com>
2026-03-31 18:46:10 +0530

    getPatientDiseasesMedicineList

--- meditrek/server/adminAPI/controller/subAdminController.js
+++ meditrek/server/adminAPI/controller/subAdminController.js
@@ -6971,20 +6971,15 @@ const getDiseaseMedicineSummary = (req, res) => {
       Promise.all(promises)
         .then(finalPatients => {
 
-          const patientsWithMeds = finalPatients.filter(p => p.medications.length > 0);
+          const medicineCountMap = {};
+          finalPatients.forEach(p => {
+            p.medications.forEach(med => {
+              if (!medicineCountMap[med.name]) medicineCountMap[med.name] = 0;
+              medicineCountMap[med.name] += 1;
+            });
+          });
 
-const medicineCountMap = {};
-
-patientsWithMeds.forEach(p => {
-  p.medications.forEach(med => {
-    if (!medicineCountMap[med.name]) {
-      medicineCountMap[med.name] = 0;
-    }
-    medicineCountMap[med.name] += 1;
-  });
-});
-
-const matchedPatients = patientsWithMeds.length;
+          const matchedPatients = finalPatients.length;
 
           const summary = Object.keys(medicineCountMap).map(name => ({
             medicine_name: name,
