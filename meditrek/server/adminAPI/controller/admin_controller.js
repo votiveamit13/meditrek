b09af5993444d41b5fcc2710737b7b251082a0f2
Rameshwar Dhakad <dhakad7748@gmail.com>
2026-04-16 18:14:24 +0530

    breakdown

--- meditrek/server/adminAPI/controller/admin_controller.js
+++ meditrek/server/adminAPI/controller/admin_controller.js
@@ -11543,8 +11543,8 @@ const paginatedMedicineSummary = medicineSummary.slice(
             total_patients: totalPatients,
             matched_patients: matchedCount,
             percentage,
-            //age_breakdown,
-            //sex_breakdown,
+            age_breakdown,
+            sex_breakdown,
             selected_medication_count: Array.isArray(medication)
               ? medication.length
               : 0,
